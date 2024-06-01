cb8e8445dca66a6209a328ca74a2ed4afb067a84
vibhaw raj <vibhawraj494@gmail.com>
2024-06-01 16:33:23 +0530

    [Feat]:ui

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Button, TextField, Container, Typography, Box, Divider, Link } from '@mui/material';
+
+import { Button, TextField, Container, Typography, Box, Divider, Link, Grid } from '@mui/material';
 import { useState } from 'react';
 import { account } from "@/appwrite/config";
 import { useRouter } from 'next/navigation';
@@ -23,75 +24,124 @@ const Login = () => {
   };
 
   return (
-    <Container component="main" maxWidth="sm" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-      <Box sx={{ boxShadow: 3, p: 4, bgcolor: 'white', borderRadius: '0px 16px 16px 16px' }}>
-        <Typography component="h1" variant="h4" align="center" gutterBottom>
-          Welcome Back!
-        </Typography>
-        <Typography variant="subtitle1" align="center" gutterBottom>
-          <Divider>Sign In to Your Account</Divider>
-        </Typography>
-        <form onSubmit={handleLogin} style={{ marginTop: '1rem' }}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            onChange={handleChange}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={handleChange}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            sx={{ mt: 2, mb: 2, bgcolor: '#1F64FF' }}
-          >
-            Sign In
-          </Button>
-           <Box textAlign="center" sx={{ mt: 2, mb: 2 }}>
-          OR
+    <Container
+      component="main"
+      maxWidth="md"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+        fontFamily: 'Nunito',
+      }}
+    >
+      <Box
+        sx={{
+          boxShadow: 3,
+          p: 0,
+          bgcolor: 'white',
+          borderRadius: '0px 16px 0px 16px',
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          width: '100%',
+        }}
+      >
+        <Box sx={{ flex: 1, p: 4 }}>
+          <Typography component="h1" variant="h4" align="center" gutterBottom sx={{ fontWeight: 'bold', fontFamily: 'Nunito' }}>
+            Welcome Back!
+          </Typography>
+          <Typography variant="subtitle1" align="center" gutterBottom sx={{ fontFamily: 'Nunito' }}>
+            <Divider>Sign In to Your Account</Divider>
+          </Typography>
+          <form onSubmit={handleLogin} style={{ marginTop: '1rem' }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleChange}
+              sx={{ fontFamily: 'Nunito' }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+              sx={{ fontFamily: 'Nunito' }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2, mb: 2, bgcolor: '#1F64FF', fontFamily: 'Nunito' }}
+            >
+              Sign In
+            </Button>
+            <Box textAlign="center" sx={{ mt: 2, mb: 2, fontFamily: 'Nunito' }}>
+              OR
+            </Box>
+            <Grid container spacing={0} justifyContent="center">
+              <Grid item xs={12} sm={6}>
+                <Button
+                  fullWidth
+                  variant="outlined"
+                  color="secondary"
+                  endIcon={<img src="/images/google.png" alt="Google Icon" style={{ width: '20px', height: '20px' }} />}
+                  onClick={() => window.location.href = '/api/auth/google'}
+                  sx={{
+                    borderColor: 'rgba(0, 0, 0, 0.23)',
+                    textTransform: 'none',
+                    ':hover': {
+                      boxShadow: 3,
+                    },
+                    fontFamily: 'Nunito',
+                  }}
+                >
+                  Login with Google
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  fullWidth
+                  variant="outlined"
+                  color="secondary"
+                  endIcon={<img src="/images/github.png" alt="GitHub Icon" style={{ width: '15px', height: '15px' }} />}
+                  onClick={() => window.location.href = '/api/auth/github'}
+                  sx={{
+                    borderColor: 'rgba(0, 0, 0, 0.23)',
+                    textTransform: 'none',
+                    ':hover': {
+                      boxShadow: 3,
+                    },
+                    fontFamily: 'Nunito',
+                  }}
+                >
+                  Login with GitHub
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+          <Typography variant="body2" align="center" sx={{ mt: 2, fontFamily: 'Nunito' }}>
+            Don't have an Account? <Link href="/signup" sx={{ fontFamily: 'Nunito' }}>SIGN UP</Link>
+          </Typography>
         </Box>
-          <Button
-            fullWidth
-            variant="outlined"
-            color="secondary"
-            startIcon={<img src="/google-icon.svg" alt="Google Icon" style={{ width: '20px', height: '20px' }} />}
-            sx={{ mt: 2 }}
-            onClick={() => window.location.href = '/api/auth/google'}
-          >
-            Sign In with Google
-          </Button>
-          <Button
-            fullWidth
-            variant="outlined"
-            color="secondary"
-            startIcon={<img src="/github-icon.svg" alt="GitHub Icon" style={{ width: '20px', height: '20px' }} />}
-            sx={{ mt: 2 }}
-            onClick={() => window.location.href = '/api/auth/github'}
-          >
-            Sign In with GitHub
-          </Button>
-        </form>
-        <Typography variant="body2" align="center" sx={{ mt: 2 }}>
-          Don't have an Account? <Link href="/signup">SIGN UP</Link>
-        </Typography>
+       <Divider orientation="vertical" flexItem sx={{ display: { xs: 'none', md: 'block' }, height: '30rem'}} />
+        <Box sx={{ flex: 1, display: { xs: 'none', md: 'block' }, position: 'relative' }}>
+          <img src="/images/Vector.png" alt="Vector" style={{ position: 'absolute', bottom: 0, right: 0, width: '100%' }} />
+        </Box>
       </Box>
     </Container>
   );
--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -41,138 +41,162 @@ const Signup = () => {
   };
 
   return (
-    
     <Container
       component="main"
-      maxWidth="sm"
+      maxWidth="md"
       sx={{
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         minHeight: '100vh',
+        fontFamily: 'Nunito',
       }}
     >
-      
- <Box
+      <Box
         sx={{
           boxShadow: 3,
-          p: 4,
+          p: 0,
           bgcolor: 'white',
-          borderRadius: '0px 16px 16px 16px',
-          width: '100%'
+          borderRadius: '0px 16px 0px 16px',
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          width: '100%',
         }}
       >
-        <Typography component="h1" variant="h5" align="center" gutterBottom>
-          Hello!
-        </Typography>
-        <Typography variant="subtitle1" align="center" gutterBottom>
-          Create Your Account
-        </Typography>
-        <form onSubmit={handleSignup} style={{ marginTop: '1rem' }}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="firstname"
-            label="First Name"
-            name="firstname"
-            autoComplete="firstname"
-            autoFocus
-            onChange={handleChange}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="lastname"
-            label="Last Name"
-            name="lastname"
-            autoComplete="lastname"
-            onChange={handleChange}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            onChange={handleChange}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={handleChange}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="confirmPassword"
-            label="Confirm Password"
-            type="password"
-            id="confirmPassword"
-            autoComplete="current-password"
-            onChange={handleChange}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            sx={{ mt: 2, mb: 2, bgcolor: '#1F64FF' }}
-          >
-            Sign Up
-          </Button>
-        </form>
-        <Box textAlign="center" sx={{ mt: 0, mb: 2 }}>
-          OR
+        <Box sx={{ flex: 1, p: 4}}>
+          <Typography component="h1" variant="h4" align="center" gutterBottom sx={{ fontWeight: 'bold', fontFamily: 'Nunito' }}>
+            Hello!
+          </Typography>
+          <Typography variant="subtitle1" align="center" gutterBottom sx={{ fontFamily: 'Nunito' }}>
+            <Divider>Create Your Account</Divider>
+          </Typography>
+          <form onSubmit={handleSignup} style={{ marginTop: '1rem' }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="firstname"
+              label="First Name"
+              name="firstname"
+              autoComplete="firstname"
+              autoFocus
+              onChange={handleChange}
+              sx={{ fontFamily: 'Nunito' }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="lastname"
+              label="Last Name"
+              name="lastname"
+              autoComplete="lastname"
+              onChange={handleChange}
+              sx={{ fontFamily: 'Nunito' }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              onChange={handleChange}
+              sx={{ fontFamily: 'Nunito' }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+              sx={{ fontFamily: 'Nunito' }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="confirmPassword"
+              label="Confirm Password"
+              type="password"
+              id="confirmPassword"
+              autoComplete="current-password"
+              onChange={handleChange}
+              sx={{ fontFamily: 'Nunito' }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2, mb: 2, bgcolor: '#1F64FF', fontFamily: 'Nunito' }}
+            >
+              Sign Up
+            </Button>
+          </form>
+          <Box textAlign="center" sx={{ mt: 0, mb: 2, fontFamily: 'Nunito' }}>
+            OR
+          </Box>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={12} sm={6}>
+              <Button
+                fullWidth
+                variant="outlined"
+                color="secondary"
+                endIcon={<img src="/images/google.png" alt="Google Icon" style={{ width: '20px', height: '20px' }} />}
+                onClick={() => window.location.href = '/api/auth/google'}
+                sx={{
+                  borderColor: 'rgba(0, 0, 0, 0.23)',
+                  ':hover': {
+                    boxShadow: 3,
+                  },
+                  fontFamily: 'Nunito',
+                }}
+              >
+                Sign Up with Google
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button
+                fullWidth
+                variant="outlined"
+                color="secondary"
+                endIcon={<img src="/images/github.png" alt="GitHub Icon" style={{ width: '20px', height: '20px' }} />}
+                onClick={() => window.location.href = '/api/auth/github'}
+                sx={{
+                  borderColor: 'rgba(0, 0, 0, 0.23)',
+                  ':hover': {
+                    boxShadow: 3,
+                  },
+                  fontFamily: 'Nunito',
+                }}
+              >
+                Sign Up with GitHub
+              </Button>
+            </Grid>
+          </Grid>
+          <Typography variant="body2" align="center" sx={{ mt: 2, fontFamily: 'Nunito' }}>
+            Already have an Account? <Link href="/login" sx={{ fontFamily: 'Nunito' }}>LOGIN</Link>
+          </Typography>
         </Box>
-        <Grid container spacing={2} justifyContent="center">
-          <Grid item xs={6}>
-            <Button
-              variant="outlined"
-              color="secondary"
-              startIcon={<img src="/google-icon.svg" alt="Google Icon" style={{ width: '15px', height: '15px' }} />}
-              onClick={() => window.location.href = '/api/auth/google'}
-            >
-              Sign Up with Google
-            </Button>
-          </Grid>
-          <Grid item xs={6}>
-            <Button
-              variant="outlined"
-              color="secondary"
-              startIcon={<img src="/github-icon.svg" alt="GitHub Icon" style={{ width: '15px', height: '15px' }} />}
-              onClick={() => window.location.href = '/api/auth/github'}
-            >
-              Sign Up with GitHub
-            </Button>
-          </Grid>
-        </Grid>
-        <Typography variant="body2" align="center" sx={{ mt: 2 }}>
-          Already have an Account? <Link href="/login">LOGIN</Link>
-        </Typography>
+        <Divider orientation="vertical" flexItem sx={{ display: { xs: 'none', md: 'block' } }} />
+        <Box sx={{ flex: 1, display: { xs: 'none', md: 'block' }, position: 'relative' }}>
+          <img src="/images/Vector.png" alt="Vector" style={{ position: 'absolute', bottom: 0, right: 0, width: '100%' }} />
+        </Box>
       </Box>
-   
-     
-     
     </Container>
   );
 };
 
 export default Signup;
-  
